19366b6414912a64578624ad3a2751797bd94922
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-05-27 03:41:59 +0530

    Trusted Tech

---
+++ build/precache-manifest.1217053547cd15af603ecc5dd17ddfee.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a406004df69815dedc7c81918e23b068",
+    "url": "/TrustedTech/index.html"
+  },
+  {
+    "revision": "ced99849b88947dab11c",
+    "url": "/TrustedTech/static/css/main.f1ccde92.chunk.css"
+  },
+  {
+    "revision": "03d31ec511aa9ce51351",
+    "url": "/TrustedTech/static/js/2.36e3da45.chunk.js"
+  },
+  {
+    "revision": "7644e844781332392746516356349236",
+    "url": "/TrustedTech/static/js/2.36e3da45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced99849b88947dab11c",
+    "url": "/TrustedTech/static/js/main.cf2b7dd2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/TrustedTech/static/js/main.cf2b7dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3c5e1d8e0ea02ac664",
+    "url": "/TrustedTech/static/js/runtime-main.7eb05bdb.js"
+  },
+  {
+    "revision": "4a40f39c87b10e633d9b506c8d6e41b6",
+    "url": "/TrustedTech/static/media/Grain.4a40f39c.jpg"
+  },
+  {
+    "revision": "71ffaa02b8a349b4a941bf1b6f3f60b0",
+    "url": "/TrustedTech/static/media/addProduct.71ffaa02.png"
+  },
+  {
+    "revision": "4a633ef101cc1482d94242c82291531e",
+    "url": "/TrustedTech/static/media/buy.4a633ef1.png"
+  },
+  {
+    "revision": "82fa206b257a54a950c795c83ad5fcf8",
+    "url": "/TrustedTech/static/media/explore.82fa206b.png"
+  },
+  {
+    "revision": "01750d7ce06ab7678b883c90e4974287",
+    "url": "/TrustedTech/static/media/farmer.01750d7c.png"
+  },
+  {
+    "revision": "6002450e78b69fff7f4da4c1f030ae1f",
+    "url": "/TrustedTech/static/media/penny.6002450e.png"
+  },
+  {
+    "revision": "184b7313e46a4c4ee6195486f97addfb",
+    "url": "/TrustedTech/static/media/upload.184b7313.png"
+  }
+]);
