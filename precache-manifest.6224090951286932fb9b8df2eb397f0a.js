b6723c78924b8a33cf2100f83eb9d51b06a1764d
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-05-28 23:00:06 +0530

    Updates

---
+++ precache-manifest.6224090951286932fb9b8df2eb397f0a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ced9fddce19269572d8bfe46459ecbd",
+    "url": "/Agro/index.html"
+  },
+  {
+    "revision": "bd4666d3844aff9c4c82",
+    "url": "/Agro/static/css/main.07eda984.chunk.css"
+  },
+  {
+    "revision": "61cb097c215947fd0e4a",
+    "url": "/Agro/static/js/2.d8417c2c.chunk.js"
+  },
+  {
+    "revision": "7644e844781332392746516356349236",
+    "url": "/Agro/static/js/2.d8417c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4666d3844aff9c4c82",
+    "url": "/Agro/static/js/main.4bdc8eba.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Agro/static/js/main.4bdc8eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b56a57b96623705343",
+    "url": "/Agro/static/js/runtime-main.aaee32ec.js"
+  },
+  {
+    "revision": "4a40f39c87b10e633d9b506c8d6e41b6",
+    "url": "/Agro/static/media/Grain.4a40f39c.jpg"
+  },
+  {
+    "revision": "71ffaa02b8a349b4a941bf1b6f3f60b0",
+    "url": "/Agro/static/media/addProduct.71ffaa02.png"
+  },
+  {
+    "revision": "4a633ef101cc1482d94242c82291531e",
+    "url": "/Agro/static/media/buy.4a633ef1.png"
+  },
+  {
+    "revision": "82fa206b257a54a950c795c83ad5fcf8",
+    "url": "/Agro/static/media/explore.82fa206b.png"
+  },
+  {
+    "revision": "01750d7ce06ab7678b883c90e4974287",
+    "url": "/Agro/static/media/farmer.01750d7c.png"
+  },
+  {
+    "revision": "6002450e78b69fff7f4da4c1f030ae1f",
+    "url": "/Agro/static/media/penny.6002450e.png"
+  },
+  {
+    "revision": "184b7313e46a4c4ee6195486f97addfb",
+    "url": "/Agro/static/media/upload.184b7313.png"
+  }
+]);
